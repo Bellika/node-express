1194073f4da0d7f06c333d0f31622940f49fb468
Bellika <markusbellika@gmail.com>
2024-11-11 14:09:43 +0100

    index file in res

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`)
 })
